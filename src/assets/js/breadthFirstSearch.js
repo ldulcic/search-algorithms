5491de076a4a650dc06f0546d107e0e5183b3809
ldulcic <luka.dulcic@fer.hr>
2015-05-16 14:13:56 +0200

    breadthFirstSearch(nije jos gotov)

--- src/assets/js/breadthFirstSearch.js
+++ src/assets/js/breadthFirstSearch.js
@@ -2,7 +2,9 @@ function BreadthFirstSearch (startNode, endNode) {
 	this.startNode = startNode;
 	this.endNode = endNode;
 	this.openedNodes = [];
-	this.nextStep = null;
+	this.expand(startNode.expand());
+	if(openedNodes.length == 0) return null;
+	this.nextStep = openedNodes.pop();
 }
 
 BreadthFirstSearch.prototype = {
@@ -10,6 +12,21 @@ BreadthFirstSearch.prototype = {
 	constructor: BreadthFirstSearch,
 
 	nextStep: function (node) {
-		if(this.nextStep == endNode && node == endNode)
+		if(this.nextStep == endNode && node == endNode) {
+			//kraj
+		}
+
+		if(nextStep != node) {
+			return false;
+		}
+
+		this.expand(node.expand());
+		this.nextStep = openedNodes.pop();
+	},
+
+	expand: function (nodes) {
+		for (var i = nodes.length - 1; i >= 0; i--) {
+			openedNodes.splice(0, 0, nodes[i]);
+		};
 	}	
 }
